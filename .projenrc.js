c842645e7bf0647b14d8f4637902fb58f47e43b3
Josh Kellendonk <misterjoshua@users.noreply.github.com>
2022-01-21 16:40:54 -0700

    chore: upgrade projen (#659)

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,20 @@
-const {
-  AwsCdkConstructLibrary,
-  DependenciesUpgradeMechanism, Semver,
-} = require('projen');
+const { awscdk } = require('projen');
 
-const project = new AwsCdkConstructLibrary({
+const cdkDependencies = [
+  '@aws-cdk/aws-cognito',
+  '@aws-cdk/aws-ec2',
+  '@aws-cdk/aws-efs',
+  '@aws-cdk/aws-ecs',
+  '@aws-cdk/aws-secretsmanager',
+  '@aws-cdk/aws-elasticloadbalancingv2',
+  '@aws-cdk/aws-elasticloadbalancingv2-targets',
+  '@aws-cdk/aws-logs',
+  '@aws-cdk/core',
+];
+
+const project = new awscdk.AwsCdkConstructLibrary({
   name: '@wheatstalk/cdk-ecs-website',
   description: 'CDK ECS Website Constructs',
-  jsiiVersion: Semver.caret('1.7.0'),
   authorName: 'Josh Kellendonk',
   authorEmail: 'joshkellendonk@gmail.com',
   repository: 'git@github.com:wheatstalk/cdk-ecs-website.git',
@@ -15,8 +23,7 @@ const project = new AwsCdkConstructLibrary({
   projenUpgradeSecret: 'YARN_UPGRADE_TOKEN',
   autoApproveUpgrades: true,
   autoApproveOptions: {
-    secret: 'GITHUB_TOKEN',
-    allowedUsernames: ['github-actions', 'github-actions[bot]', 'misterjoshua'],
+    allowedUsernames: ['misterjoshua'],
   },
 
   defaultReleaseBranch: 'master',
@@ -44,22 +51,12 @@ const project = new AwsCdkConstructLibrary({
 
   cdkVersion: '1.68.0',
   cdkDependenciesAsDeps: false, // https://dev.to/aws-builders/correctly-defining-dependencies-in-l3-cdk-constructs-45p#update-20210417
+  cdkDependencies: cdkDependencies,
   cdkTestDependencies: [
     'aws-cdk',
     '@aws-cdk/assert',
     '@aws-cdk/aws-rds',
-  ],
-  cdkDependencies: [
-    '@aws-cdk/aws-cognito',
-    '@aws-cdk/aws-ec2',
-    '@aws-cdk/aws-efs',
-    '@aws-cdk/aws-ecs',
-    '@aws-cdk/aws-secretsmanager',
-    '@aws-cdk/aws-elasticloadbalancingv2',
-    '@aws-cdk/aws-elasticloadbalancingv2-targets',
-    '@aws-cdk/aws-logs',
-    '@aws-cdk/core',
-  ],
+  ].concat(cdkDependencies),
   npmignore: ['node_modules'],
   compat: true,
 });
