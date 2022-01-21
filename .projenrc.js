89f94db687f26b32cef12c66c7de5328ffefcf60
Josh Kellendonk <joshkellendonk@gmail.com>
2022-01-21 16:28:03 -0700

    fix build

--- .projenrc.js
+++ .projenrc.js
@@ -1,5 +1,17 @@
 const { awscdk } = require('projen');
 
+const cdkDependencies = [
+  '@aws-cdk/aws-cognito',
+  '@aws-cdk/aws-ec2',
+  '@aws-cdk/aws-efs',
+  '@aws-cdk/aws-ecs',
+  '@aws-cdk/aws-secretsmanager',
+  '@aws-cdk/aws-elasticloadbalancingv2',
+  '@aws-cdk/aws-elasticloadbalancingv2-targets',
+  '@aws-cdk/aws-logs',
+  '@aws-cdk/core',
+];
+
 const project = new awscdk.AwsCdkConstructLibrary({
   name: '@wheatstalk/cdk-ecs-website',
   description: 'CDK ECS Website Constructs',
@@ -39,22 +51,12 @@ const project = new awscdk.AwsCdkConstructLibrary({
 
   cdkVersion: '1.68.0',
   cdkDependenciesAsDeps: false, // https://dev.to/aws-builders/correctly-defining-dependencies-in-l3-cdk-constructs-45p#update-20210417
+  cdkDependencies: cdkDependencies,
   cdkTestDependencies: [
     'aws-cdk',
     '@aws-cdk/assert',
     '@aws-cdk/aws-rds',
-  ],
-  cdkDependencies: [
-    '@aws-cdk/aws-cognito',
-    '@aws-cdk/aws-ec2',
-    '@aws-cdk/aws-efs',
-    '@aws-cdk/aws-ecs',
-    '@aws-cdk/aws-secretsmanager',
-    '@aws-cdk/aws-elasticloadbalancingv2',
-    '@aws-cdk/aws-elasticloadbalancingv2-targets',
-    '@aws-cdk/aws-logs',
-    '@aws-cdk/core',
-  ],
+  ].concat(cdkDependencies),
   npmignore: ['node_modules'],
   compat: true,
 });
